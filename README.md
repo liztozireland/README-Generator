ae0130d6efb2b100435c52573009e6867e12d35c
Liz Ireland <liz.t.ireland@gmail.com>
2021-11-06 18:53:48 -0700

    final test before video

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# askdjfh;
-## License
-MIT
-[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+# slkfjda
+
+[![License](https://img.shields.io/badge/License-none-blue.svg)](https://opensource.org/licenses/none)
 ## Description 
-aslkdfj
+laskdjfa
 ## Description
-Our motivation to start this project: kwjehAF
+Our motivation to start this project: lasdjkf
 ## What Problem do We Want to Solve?
-We want to solve: adfsafsd  
+We want to solve: sladkjf  
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
@@ -18,18 +17,16 @@ We want to solve: adfsafsd
 * [Questions](#questions)
 ## Installation]
 The following necessary dependencies need to be installed to properly run this application:
-alsdfasdfa
+sladjfk
 ## Instructions and Examples for Use:
-asdfsadf
+asldkfj
 ## Collaborators, Third-Party APIs, and Tutorials Used:
-adfa
-## Liscense:
-## License
-MIT
-[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+lskadjfa
+## License:
+
 ## Test Instructions:
-asdfasd
+lsakdjfsadj
 ## Github Link:
-afdaasf
+klsadjfjak
 ## If you have any questions, email me!
-asdffa
+lsadfjk
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,12 +2,20 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) { {
   return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`}
-  { return `[License: MIT](https://img.shields.io/badge/License-${license}-MIT-yellow.svg)](https://opensource.org/licenses/${license})`} 
-  { return `[![License](https://img.shields.io/badge/License-${license}-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/${license})`} }
+  { return `[License](https://img.shields.io/badge/License-${license}-MIT-yellow.svg)](https://opensource.org/licenses/${license})`} 
+  { return `[![License](https://img.shields.io/badge/License-${license}-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/${license})`} 
+ }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {
+//   if (license === "none") {
+//     return "" 
+//   } else {
+//     return `## License
+// ${license}`
+//   }
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -46,9 +54,8 @@ ${data.installation}
 ${data.usage}
 ## Collaborators, Third-Party APIs, and Tutorials Used:
 ${data.credits}
-## Liscense:
+## License:
 ${renderLicenseSection(data.license)}
-${renderLicenseBadge(data.license)}
 ## Test Instructions:
 ${data.test}
 ## Github Link:
